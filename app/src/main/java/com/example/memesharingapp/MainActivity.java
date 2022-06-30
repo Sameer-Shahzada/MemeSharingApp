b82ca45272f492fef9fd808d1bcc33f971b11316
Shahzada <md.shahzada1606@gmail.com>
2022-06-30 23:01:29 +0530

    Fix Splash Screen Code for loadMeme method Add volley library dependancy & Glide dependancy

--- app/src/main/java/com/example/memesharingapp/MainActivity.java
+++ app/src/main/java/com/example/memesharingapp/MainActivity.java
@@ -1,8 +1,19 @@
 package com.example.memesharingapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import androidx.appcompat.app.AppCompatActivity;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,8 +21,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // invoke loadMeme method
+        loadMeme();
     }
 
+    private void loadMeme(){
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://meme-api.herokuapp.com/gimme";    // store given API in url variable
+
+        // Request a string response from the provided URL.
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                            String responseResult = null;  // store default value for string variable
+                        try {
+                           responseResult = response.getString("url");
+                            System.out.println(responseResult + "Hello Sameer"); // this statement for check API response
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        ImageView img = findViewById(R.id.memeImageView);   // way of access imageView
+                        Glide.with(MainActivity.this).load(responseResult).into(img);  // Glide library
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+                        Log.d("error - ","something went wrong.");
+                    }
+                });
+        queue.add(jsonObjectRequest);
+    }
     public void shareMeme(View view) {
 
     }
@@ -19,6 +65,4 @@ public class MainActivity extends AppCompatActivity {
     public void nextMeme(View view) {
 
     }
-
-
 }
